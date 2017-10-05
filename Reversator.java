2e4dee7bed5356fcfaee46e41e57bb5ee62b4c02
Maminspapin <y.lyulchenko@mail.ru>
2017-10-05 21:42:53 +0700

    Реализация методов по перевороту строки

---
+++ Reversator.java
@@ -0,0 +1,44 @@
+package Study.Reversator;
+
+public class Reversator {
+
+    public static String reverse1 (String str) {
+
+        if (str != null) {
+
+            String reversStr = "";
+            for (int i = str.length() - 1; i >= 0; i--) {
+                reversStr += str.charAt(i);
+            }
+            return reversStr;
+        }
+        return null;
+    }
+
+    public static StringBuilder reverse2 (String str) {
+
+        if (str != null) {
+
+            StringBuilder reversStr = new StringBuilder(str);
+            int i = 0;
+            int j = str.length()-1;
+
+            while (i < j) {
+                char temp = str.charAt(i);
+                reversStr.setCharAt(i,str.charAt(j));
+                reversStr.setCharAt(j,temp);
+                i++;
+                j--;
+            }
+            reversStr.toString();
+            return reversStr;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse1("0123456789"));
+        System.out.println(reverse2("0123456789"));
+    }
+}

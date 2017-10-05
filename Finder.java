c3b23a95d8a69807f0913e78a90b5f8219be1374
Maminspapin <y.lyulchenko@mail.ru>
2017-10-05 21:13:37 +0700

    Реализация метода по поиску элемента в массиве

---
+++ Finder.java
@@ -0,0 +1,41 @@
+/*
+Поиск элемента в массиве
+*/
+
+package Study.Finder;
+
+public class Finder {
+
+    public static int findIdx(int[] arr, int el) {
+
+        if (arr != null) {
+
+            int leftVal = 0;
+            int rightVal = arr.length;
+            int middleVal;
+
+        while (leftVal <= rightVal) {
+
+                middleVal = leftVal + (rightVal - leftVal)/2;
+
+                if (el == arr[middleVal]) return middleVal;
+                else if (el < arr[middleVal]) {
+                    rightVal = middleVal-1;
+                }
+                else if (el > arr[middleVal]) {
+                    leftVal = middleVal+1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] myArr = {0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.println(findIdx(myArr, myArr[i]));
+        }
+    }
+}
